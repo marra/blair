96fa2f2e658ba24fdc8add55b39fc6369a909d30
Diego <diego.cabrera.mla@gmail.com>
2011-10-19 23:08:33 -0300

    Agregamos el HomeController

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -11,7 +11,7 @@ class UrlMappings {
 		}*/
 		
 		name principal: "/" {
-			controller="city"
+			controller="home"
 			action="home"
 		}
 		
--- grails-app/controllers/blair/CityController.groovy
+++ grails-app/controllers/blair/CityController.groovy
@@ -50,19 +50,7 @@ class CityController {
 		}	
 	}
 		
-	def home = {
-		
-		def country = Country.findByName("Argentina") //TODO obtener esto de la URL
-		def allStates = State.getAll().sort{it.name}
-		def topVenues = cityService.getTopVenues(Constants.NOW,country)
-		def touristicPlaces = City.findAllByIsTouristicPlace("Y")
-		Collections.shuffle(touristicPlaces)
-		
-		def model = [allStates:allStates, topVenues:topVenues, touristicPlaces:touristicPlaces.getAt(0..9), thisCountry:country,
-					 where:"country"]
 
-		render (view:"home", model:model)
-	}
 	
 	
 	def venue = {
